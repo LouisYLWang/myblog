df65b69c8e19b258cefd6eee54508029c93bbc05
Louis YL Wang <louis.yl.wang@outlook.com>
2019-10-18 00:47:58 -0700

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -25,12 +25,12 @@ class Post(models.Model):
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
     excerpt = models.CharField(max_length = 200, blank=True)
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
     views = models.PositiveIntegerField(default=0)
     
     #review = models.PositiveIntegerField(default=0)
-    #author = models.ForeignKey(User)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
