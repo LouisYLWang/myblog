cd2937d3e7342d1a9c430456f3ccab2b1936f4f8
LouisYLWang <louis.yl.wang@outlook.com>
2020-10-15 22:51:56 -0700

    fix WhiteNoise issues

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -55,11 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-MIDDLEWARE_CLASSES  = (
     'whitenoise.middleware.WhiteNoiseMiddleware'
-)
+]
 
 ROOT_URLCONF = 'blogproject.urls'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
